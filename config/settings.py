7dd843c898c83de2da8d0b05f1e638bedd85a43b
sunghyun1356 <sunghyun1356@naver.com>
2023-08-07 19:39:46 +0900

    초기환경설정 재세팅 -> secret-key는 노션확인

--- config/settings.py
+++ config/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_URL = os.getenv('BASE_URL')
 
-load_dotenv()
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -43,9 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Users',
-    'Restaurants',
-    'Reviews',
+    # 애플리케이션
+    'Users.apps.UsersConfig',
+    'Restaurants.apps.RestaurantsConfig',
+    'Reviews.apps.ReviewsConfig',
     'rest_framework',
     'corsheaders',
 ]
@@ -117,7 +119,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'Users.user'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    ] 
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
