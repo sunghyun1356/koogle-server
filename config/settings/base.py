946c6963071cf9609037dae409e1e7aed2b5d8b4
sunghyun1356 <sunghyun1356@naver.com>
2023-08-16 14:01:49 +0900

    db저장소 mysql로 생각

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from django.core.exceptions import ImproperlyConfigured
-
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
--- config/settings/local.py
+++ config/settings/local.py
@@ -17,9 +17,15 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 # 데이터베이스는 AWS RDS Mysql 사용 했습니다.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # engine: mysql
+        'NAME' : 'koogle', # DB Name
+        'USER' : 'admin', # DB User
+        'PASSWORD' : 'sunghyun1356', # Password
+        'HOST': 'sunghyun1356.catymkthpwcp.ap-northeast-2.rds.amazonaws.com', # 생성한 데이터베이스 엔드포인트
+        'PORT': '3306', # 데이터베이스 포트
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
-
+}
 STATIC_ROOT = BASE_DIR / 'static'
